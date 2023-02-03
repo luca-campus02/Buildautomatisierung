7076ed0938337db714ab9bfcd0178bec10a79e55
luca-campus02 <52205286@edu.campus02.at>
2023-02-03 19:50:37 +0100

    added maven tests

--- src/test/java/org/campus02/ronchetti/BMICalculatorTest.java
+++ src/test/java/org/campus02/ronchetti/BMICalculatorTest.java
@@ -1,9 +1,6 @@
 package org.campus02.ronchetti;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
